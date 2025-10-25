354696b14a161df931784e10ac216522f7d78e28
aman035 <guptaaman200115@gmail.com>
2025-10-25 16:19:39 +0530

    fix: remove db + unnecessary modules - shifted to envio

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
 import { HealthModule } from './health/health.module';
-import { MarketsModule } from './markets/markets.module';
-import { DatabaseModule } from './database/database.module';
 import { TweetAnalyzerModule } from './tweet-analyzer/tweet-analyzer.module';
 
 @Module({
-  imports: [DatabaseModule, HealthModule, MarketsModule, TweetAnalyzerModule],
+  imports: [HealthModule, TweetAnalyzerModule],
 })
 export class AppModule {}
